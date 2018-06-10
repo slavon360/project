8a08ea2e22a93e9702152d5200d8225c8aff81e0
slavon360 <slavon360@gmail.com>
2018-06-10 16:24:31 +0300

    Landing upd

--- src/components/Landing/WhyUse/index.js
+++ src/components/Landing/WhyUse/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import 
+import BitcoinIcon from '../../UI/Icons/BitcoinLanding';
+import PadlockIcon from '../../UI/Icons/Padlock';
 import classes from './WhyUse.css';
 
 const header = () => (
@@ -7,21 +8,21 @@ const header = () => (
     <h2>Why use Bithela</h2>
     <div className={classes.TabsWrp}>
       <div className={classes.Trade}>
-        <div className={classes.Picture}>
-          <img />
+        <div className={classes.Icon}>
+          <BitcoinIcon outerRadiusStyles={{ fill: '#932c88' }} innerRadiusStyles={{ fill: '#ce41a1' }} />
         </div>
         <div className={classes.Title}>Trade Bitcoin and Ethereum easily</div>
         <div className={classes.Description}>Join the growing world of digital currencies using our platform</div>
       </div>
       <div className={classes.Trust}>
-        <div className={classes.Picture}>
-          <img />
+        <div className={classes.Icon}>
+          <PadlockIcon topStyles={{ fill: '#ce41a1' }} bottomStyles={{ fill: '#932c88' }} />
         </div>
         <div className={classes.Title}>Trust and Security</div>
         <div className={classes.Description}>Avoid perennial inflation by buying into deflationary currencies</div>
       </div>
       <div className={classes.Buy}>
-        <div className={classes.Picture}>
+        <div className={classes.Icon}>
           <img />
         </div>
         <div className={classes.Title}>Buy and Sell in Seconds</div>
