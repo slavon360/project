e31fab5d1ad93178b632570b3aaf14bf67516c45
slavon360 <slavon360@gmail.com>
2018-06-14 11:51:54 +0300

    component/Faq fonts updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Layout from './hoc/Layout';
 // import Modal from './components/UI/Modal';
-import TermsOfUse from './components/TermsOfUse';
+// import TermsOfUse from './components/TermsOfUse';
 
 // import Welcome from './components/Modals/Welcome';
 
@@ -15,25 +15,25 @@ import TermsOfUse from './components/TermsOfUse';
 // import AuthenticatorSupport from './components/Modals/AuthenticatorSupport';
 
 // import ChangePassword from './components/Modals/ChangePassword';
-/*
-import Login from './containers/Login';
 
-import BuySell from './containers/BuySell';
-import Balances from './containers/Balances';
-import Deposit from './containers/Deposit';
-import Withdraw from './containers/Withdraw';
-import SendRequest from './containers/SendRequest';
-import Faq from './containers/FAQ';
-import Profile from './containers/Profile';
+// import Login from './containers/Login';
+
+// import BuySell from './containers/BuySell';
+// import Balances from './containers/Balances';
+// import Deposit from './containers/Deposit';
+// import Withdraw from './containers/Withdraw';
+// import SendRequest from './containers/SendRequest';
+// import Faq from './containers/FAQ';
+// import Profile from './containers/Profile';
 import Verification from './containers/Verification';
 import './App.css';
-*/
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-          <TermsOfUse />
+          <Verification />
         </Layout>
       </div>
     );
