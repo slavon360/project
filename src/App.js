4840220645cf3095d8d32fdec1a8d8461ec08eb7
slavonSlv <slava.l@heidep.com>
2018-07-18 00:18:51 +0300

    header background fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Layout from './hoc/Layout';
 // import Modal from './components/UI/Modal';
 
@@ -14,8 +14,7 @@ import Layout from './hoc/Layout';
 // import AuthenticatorSupport from './components/Modals/AuthenticatorSupport';
 
 // import ChangePassword from './components/Modals/ChangePassword';
-import Verification from './containers/Verification';
-/*
+
 import Login from './containers/Login';
 import TermsOfUse from './components/TermsOfUse';
 import BuySell from './containers/BuySell';
@@ -26,7 +25,6 @@ import SendRequest from './containers/SendRequest';
 import Faq from './containers/FAQ';
 import Profile from './containers/Profile';
 import Verification from './containers/Verification';
-*/
 import classes from './App.css';
 
 class App extends Component {
@@ -34,11 +32,23 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Layout>
-          <Verification />
+          <Switch>
+            <Route path="/buy-sell" component={BuySell} />
+            <Route path="/balances" component={Balances} />
+            <Route path="/deposit" component={Deposit} />
+            <Route path="/withdraw" component={Withdraw} />
+            <Route path="/send-request" component={SendRequest} />
+            <Route path="/help" component={Faq} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/verification" component={Verification} />
+            <Route path="/terms" component={TermsOfUse} />
+            <Route path="/terms" component={Login} />
+            <Redirect to="/buy-sell" />
+          </Switch>
         </Layout>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
